ca2d5b16d209ff8d54243a87859e54f8c6269f03
arwidmark <arwidmark@gmail.com>
2015-11-27 19:49:38 +0100

    MDT Scripts

---
+++ Install-HYDMDT2013U1.ps1
@@ -0,0 +1,32 @@
+<#
+
+************************************************************************************************************************
+
+Created:	2015-03-01
+Version:	1.1
+
+Disclaimer:
+This script is provided "AS IS" with no warranties, confers no rights and 
+is not supported by the author or DeploymentArtist.
+
+Author - Johan Arwidmark
+    Twitter: @jarwidmark
+    Blog   : http://deploymentresearch.com
+
+************************************************************************************************************************
+
+#>
+
+# Validation
+Write-Output "Checking for setup files"
+If (Test-Path C:\Setup\MDT2013U1\MicrosoftDeploymentToolkit2013_x64.msi){
+    Write-Output "Setup files found, OK, continuing..."
+    Write-Output ""
+    } 
+Else {
+    Write-Warning "Oupps, cannot setup files, aborting..."
+    Break
+}
+
+# Install MDT 2013
+& msiexec.exe /i 'C:\Setup\MDT2013\MicrosoftDeploymentToolkit2013_x64.msi' /quiet
