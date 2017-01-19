bf90c7ac200b7e2eb44c5a63cff41573ab7bc318
Chad Simmons <ChadSimmons@users.noreply.github.com>
2017-01-19 08:17:03 -0600

    Bug fix and simplification
    Removed bug of a double backslash.
    Removed unneeded specification of drive in each new-item command

--- New-HYDConfigMgrConsoleFolderStructure.ps1
+++ New-HYDConfigMgrConsoleFolderStructure.ps1
@@ -3,7 +3,7 @@
 ************************************************************************************************************************
 
 Created:	2015-03-01
-Version:	1.1
+Version:	1.2
 
 Disclaimer:
 This script is provided "AS IS" with no warranties, confers no rights and 
@@ -17,15 +17,14 @@ Author - Johan Arwidmark
 
 #>
 
-
 Import-Module $env:SMS_ADMIN_UI_PATH.Replace("\bin\i386","\bin\configurationmanager.psd1")
-$SiteCode = Get-PSDrive -PSProvider CMSITE
-Set-Location "$($SiteCode.Name):\"
+Push-Location "$($(Get-PSDrive -PSProvider CMSITE).Name):"
 
 # Create Folders in Device Collections
-New-Item -Name 'Limiting Collections' -Path "$($SiteCode.Name):\DeviceCollection"
-New-Item -Name 'OSD' -Path "$($SiteCode.Name):\DeviceCollection"
-New-Item -Name 'Software' -Path "$($SiteCode.Name):\DeviceCollection"
-New-Item -Name 'Software Updates' -Path "$($SiteCode.Name):\\DeviceCollection"
-New-Item -Name 'Compliance Settings' -Path "$($SiteCode.Name):\DeviceCollection"
+New-Item -Name 'Limiting Collections' -Path '\DeviceCollection'
+New-Item -Name 'OSD' -Path '\DeviceCollection'
+New-Item -Name 'Software' -Path '\DeviceCollection'
+New-Item -Name 'Software Updates' -Path '\DeviceCollection'
+New-Item -Name 'Compliance Settings' -Path '\DeviceCollection'
 
+Pop-Location
